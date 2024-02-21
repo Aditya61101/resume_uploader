514f1af489b36713432cdc380c7d6e64e5d24666
Aditya Kumar <adityakumarsanni.2001@gmail.com>
2024-02-21 16:35:35 +0530

    changes

--- django/resume_uploader/resume_uploader/settings.py
+++ django/resume_uploader/resume_uploader/settings.py
@@ -131,10 +131,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-# STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 # if not DEBUG:
-#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
-#     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
--- django/resume_uploader/resume_uploader/urls.py
+++ django/resume_uploader/resume_uploader/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls'))
 ]
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
